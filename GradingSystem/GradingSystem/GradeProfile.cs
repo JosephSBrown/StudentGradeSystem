1522e36eef7bb248506cea752e863e3ce5fed5fc
Joseph Brown <84574944+JosephSBrown@users.noreply.github.com>
2022-11-11 13:10:11 +0000

    Add files via upload

---
+++ GradingSystem/GradingSystem/GradeProfile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradingSystem
+{
+    class GradeProfile
+    { 
+        public static List<Grade> Grades = new List<Grade>();
+    }
+}
---
+++ GradingSystem/GradingSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showmenu = true;
+            App app = new App();
+
+            while (showmenu)
+            {
+                showmenu = app.menu();
+            }
+        }
+    }
+}
